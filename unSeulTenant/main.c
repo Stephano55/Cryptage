3aac0925b7b7b010317aa835ddd2aa860f54e0e8
Stephano55 <kevinstephano14@gmail.com>
2024-04-06 09:28:45 +0200

    décriptages et quelques petits changements

--- unSeulTenant/main.c
+++ unSeulTenant/main.c
@@ -11,20 +11,72 @@ int nombreDeChiffres(int nombre) {
     printf("Le nombre de chiffres dans %d est : %d\n", nombre, compteur);
     return compteur;
 }
-void toString(int nombre){
-    int taille = nombreDeChiffres(nombre); // Appel à nombreDeChiffres() pour obtenir la taille de la chaîne
-    char chaine[taille + 1]; // Ajout de 1 pour le caractère de fin de chaîne '\0'
+void toString(int nombre, int taille, char *chaine) {
     sprintf(chaine, "%d", nombre);
-    for(int i = 0; i < taille+1; i++){
-        printf(" %c", chaine[i]);
+    }
+
+void decripter(char *chaine, int taille){
+    for (int i = 0; i < taille; i+=2){
+        int j = i+1;
+        if (chaine[i] == '0' && chaine[j] == '0'){
+            printf(" ");
+        }
+        else if(chaine[i] == '1' && chaine[j] == '2'){
+            printf("a");
+        }
+        else if(chaine[i] == '2' && chaine[j] == '2'){
+            printf("b");
+        }
+        else if(chaine[i] == '3' && chaine[j] == '2'){
+            printf("c");
+        }
+        else if(chaine[i] == '1' && chaine[j] == '3'){
+            printf("d");
+        }
+        else if(chaine[i] == '2' && chaine[j] == '3'){
+            printf("e");
+        }
+        else if(chaine[i] == '3' && chaine[j] == '3'){
+            printf("f");
+        }
+        else if(chaine[i] == '1' && chaine[j] == '4'){
+            printf("g");
+        }
+        else if(chaine[i] == '2' && chaine[j] == '4'){
+            printf("h");
+        }
+        else if(chaine[i] == '3' && chaine[j] == '4'){
+            printf("i");
+        }
+        else if(chaine[i] == '1' && chaine[j] == '5'){
+            printf("j");
+        }
+        else if(chaine[i] == '2' && chaine[j] == '5'){
+            printf("k");
+        }
+        else if(chaine[i] == '3' && chaine[j] == '5'){
+            printf("l");
+        }
+        else if(chaine[i] == '1' && chaine[j] == '6'){
+            printf("m");
+        }
+        else if(chaine[i] == '2' && chaine[j] == '6'){
+            printf("n");
+        }
+         else if(chaine[i] == '3' && chaine[j] == '6'){
+            printf("o");
+        }
     }
 }
 int main() {
-    int nbrRepetition[3] = {1, 2, 3};
-    int codeChar[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    char lettres[26] = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    int nbr;
+     int nbr;
+    printf("Entrez un nombre : ");
+    scanf("%d", &nbr);
 
+    int taille = nombreDeChiffres(nbr);
+    char chaine[taille + 1]; // Ajoutez 1 pour le terminateur nul
+    toString(nbr, taille, chaine);
 
+    decripter(chaine, taille);
     return 0;
 }

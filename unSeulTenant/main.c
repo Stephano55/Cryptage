87983a532a76a37c46aca3dc9b9cc56615e50a5e
Stephano55 <kevinstephano14@gmail.com>
2024-04-06 08:16:31 +0200

    ajout des procédures

--- unSeulTenant/main.c
+++ unSeulTenant/main.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+int nombreDeChiffres(int nombre) {
+    int compteur = 0;
 
+    // Compter le nombre de chiffres
+    while (nombre != 0) {
+        nombre = nombre / 10; // Retrait du dernier chiffre
+        compteur++; // Incrémenter le compteur de chiffres
+    }
+    printf("Le nombre de chiffres dans %d est : %d\n", nombre, compteur);
+    return compteur;
+}
+void toString(int nombre){
+    int taille = nombreDeChiffres(nombre); // Appel à nombreDeChiffres() pour obtenir la taille de la chaîne
+    char chaine[taille + 1]; // Ajout de 1 pour le caractère de fin de chaîne '\0'
+    sprintf(chaine, "%d", nombre);
+    for(int i = 0; i < taille+1; i++){
+        printf(" %c", chaine[i]);
+    }
+}
 int main() {
     int nbrRepetition[3] = {1, 2, 3};
     int codeChar[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     char lettres[26] = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     int nbr;
-    int compteur = 0;
-    int i =0;
 
-    printf("Entrez le codage: \n");
-    scanf("%d", &nbr);
-    // Compter le nombre de chiffres dans le code
-    int temp = nbr;
-    while (temp != 0) {
-        temp = temp / 10;
-        compteur++;
-    }
-
-    printf("Le nombre de chiffres dans le code est : %d\n", compteur);
-    int taille = compteur;
-    int chfr[taille];
-    while(nbr != 0 && i < taille){
-        chfr[i] = nbr % 10;
-        nbr = nbr / 10;
-        i++;
-    }
-     printf("Les chiffres du nombre sont :\n");
-    for (int j = i - 1; j >= 0; j--) {
-        printf("%d ", chfr[j]);
-    }
-    printf("\n");
 
     return 0;
 }
